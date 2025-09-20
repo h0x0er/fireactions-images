b81305d821182feae17c343326eef326f532dfbe
Jatin <jatink843@protonmail.com>
2025-09-20 20:54:31 +0530

    using int-pr agent

--- ubuntu24.04/scripts/install-agent.sh
+++ ubuntu24.04/scripts/install-agent.sh
@@ -4,26 +4,41 @@ set -e
 mkdir -p /home/agent
 cd /home/agent
 
-# 2. Download agent binary and store it at /home/agent
 architecture=$(uname -m)
-if [ "$architecture" = "aarch64" ]; then
-    agent_tar="harden-runner_1.6.23_linux_arm64.tar.gz"
+
+# set true: to use agent-binaries from int-pr build
+int_pr="true"
+
+if [[ $int_pr == "true" ]]; then
+
+    agent=""
+    if [ "$architecture" = "aarch64" ]; then
+        agent="agent-arm"
+    else
+        agent="agent"
+    fi
+    curl -o agent "https://step-security-agent.s3.us-west-2.amazonaws.com/refs/heads/self-hosted/int/$agent"
+    curl -O "https://step-security-agent.s3.us-west-2.amazonaws.com/refs/heads/self-hosted/int/agent.service"
+
 else
-    agent_tar="harden-runner_1.6.23_linux_amd64.tar.gz"
+
+    if [ "$architecture" = "aarch64" ]; then
+        agent_tar="harden-runner_1.6.23_linux_arm64.tar.gz"
+    else
+        agent_tar="harden-runner_1.6.23_linux_amd64.tar.gz"
+    fi
+
+    curl -O "https://packages.stepsecurity.io/self-hosted/$agent_tar"
+
+    if [ "$architecture" = "aarch64" ]; then
+        echo "a3c89271e697ab39557ba8011cac7a2df690b5d27b4584d5d5abdf8845a6ce6c  $agent_tar" | sha256sum -c
+    else
+        echo "2430b850e0e4d67a2f3b626f02d2827226ee16406da6af0c47ae7b18e18bd2b8  $agent_tar" | sha256sum -c
+    fi
+
+    tar -xzf "$agent_tar"
+
 fi
-
-curl -O "https://packages.stepsecurity.io/self-hosted/$agent_tar"
-
-# 3. Confirm checksum
-if [ "$architecture" = "aarch64" ]; then
-    echo "a3c89271e697ab39557ba8011cac7a2df690b5d27b4584d5d5abdf8845a6ce6c  $agent_tar" | sha256sum -c
-else
-    echo "2430b850e0e4d67a2f3b626f02d2827226ee16406da6af0c47ae7b18e18bd2b8  $agent_tar" | sha256sum -c
-fi
-
-# 4. Extract tar
-tar -xzf "$agent_tar"
-
 
 # 5. Create /home/agent/agent.json.
 cat <<EOL >/home/agent/agent.json
